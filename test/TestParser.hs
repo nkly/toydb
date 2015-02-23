f369a1630682843d7efaf8baf0db1b561b8e0593
Nikolay Obedin <dancingrobot84@gmail.com>
2015-02-23 20:18:47 +0300

    Add tests for remaining commands

--- test/TestParser.hs
+++ test/TestParser.hs
@@ -82,5 +82,35 @@ testSelect = testGroup "SELECT"
         (Select All ["test"] (Just $ wc "a" `Gt` wi 10) (Just 5))
     ]
 
+testCreateDrop = testGroup "CREATE and DROP"
+    [ testParse
+        "create table test (foo int, bar double, baz varchar(10));"
+        (CreateTable "test"
+            [("foo", ColInt), ("bar", ColDouble), ("baz", ColVarchar 10)])
+    , testParse
+        "create index test on test (foo, bar);"
+        (CreateIndex "test" "test" ["foo", "bar"])
+    , testParse
+        "drop table test;"
+        (DropTable "test")
+    , testParse
+        "drop index test;"
+        (DropIndex "test")
+    ]
 
-testParser = testGroup "Parser" [testSelect]
+testInsertUpdateDelete = testGroup "INSERT, UPDATE, DELETE"
+    [ testParse
+        "insert into test values (1, 20.0, 'some str');"
+        (Insert "test" [VInt 1, VDouble 20.0, VString "some str"])
+    , testParse
+        "update test set (foo=1, bar=2.1, baz='some str');"
+        (Update "test"
+            [("foo", VInt 1), ("bar", VDouble 2.1), ("baz", VString "some str")]
+            Nothing)
+    , testParse
+        "delete from test;"
+        (Delete "test" Nothing)
+    ]
+
+testParser = testGroup "Parser"
+    [ testSelect, testCreateDrop, testInsertUpdateDelete ]
