f8ad01ad7211049ea053f7de6781a705762f7565
Nikolay Obedin <dancingrobot84@gmail.com>
2015-11-01 22:36:40 +0300

    Add tests for pager's reading functionality

---
+++ test/MockIO.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, TypeFamilies #-}
+module MockIO where
+
+import Control.Monad.Catch
+import Control.Monad.State (StateT)
+import Control.Monad.State.Class (MonadState)
+import Database.Toy.Internal.Util.HasFileIO
+import qualified Data.ByteString.Char8 as B
+import qualified Control.Monad.State as ST
+import qualified System.IO as S
+
+data MockIOState = MockIOState
+    { mockContents :: B.ByteString
+    , mockCurrentPos :: Int
+    }
+
+newtype MockIO a = MockIO (StateT MockIOState IO a)
+    deriving (Functor, Applicative, Monad, MonadState MockIOState)
+
+instance HasFileIO MockIO where
+    type Handle MockIO = ()
+    hOpen _ _ = return ()
+    hGet _ n = do
+        pos <- ST.gets mockCurrentPos
+        len <- fmap B.length $ ST.gets mockContents
+        if pos > len
+            then error "End of file"
+            else fmap (B.take n . B.drop pos) $ ST.gets mockContents
+    hPut _ str = do
+        pos <- ST.gets mockCurrentPos
+        contents <- ST.gets mockContents
+        let diff = pos - B.length contents
+            paddedContents = if diff > 0
+                then B.append contents $ B.replicate diff '\0'
+                else contents
+            (cBefore, cAfter) = B.splitAt pos paddedContents
+            lenToWrite = B.length str
+            newAfter = B.append str $ B.drop lenToWrite cAfter
+        ST.modify (\s -> s { mockContents = B.append cBefore newAfter })
+    hSeek _ mode offset = do
+        let offsetInt = fromInteger offset
+            action = case mode of
+                S.AbsoluteSeek -> \s -> s { mockCurrentPos = offsetInt }
+                S.RelativeSeek -> \s -> s { mockCurrentPos = mockCurrentPos s + offsetInt }
+                S.SeekFromEnd  -> \s -> s { mockCurrentPos = mockCurrentPos s - offsetInt }
+        ST.modify action
+
+instance MonadThrow MockIO where
+    throwM exc = MockIO $ throwM exc
+
+evalMockIO :: MockIO a -> MockIOState -> IO a
+evalMockIO (MockIO action) = ST.evalStateT action
+
--- test/Pager.hs
+++ test/Pager.hs
@@ -5,6 +5,7 @@ import Data.Serialize
 import Data.Word
 import Database.Toy.Internal.Pager.Trans
 import Database.Toy.Internal.Pager.Types
+import MockIO
 import Test.Hspec
 import qualified Data.ByteString.Char8 as B
 
@@ -39,11 +40,50 @@ testPageSerialization = do
                              , testPayload
                              ]
 
-testPagerReading =
-    it "should read pages" pending
+testPagerReading = do
+    it "should throw an error trying to read page with NoPageId" $
+        (testReadPage pagerConf correctContents NoPageId) `shouldThrow` anyErrorCall
+    it "should throw an exception trying to read non-existent page" $
+        (testReadPage pagerConf correctContents $ PageId 42) `shouldThrow` anyPagerException
+    it "should throw an exception trying to read page which id does not match given one" $
+        (testReadPage pagerConf incorrectContents $ PageId 0) `shouldThrow` anyPagerException
+    it "should correctly read existent page" $
+        (testReadPage pagerConf correctContents $ PageId 0) `shouldReturn` True
+    it "should correctly read existent page even with non-zero offset" $
+        (testReadPage pagerConfWithOffset contentsWithOffset $ PageId 0) `shouldReturn` True
+  where
+    testReadPage pagerConf contents pageId = do
+        let mockState = MockIOState contents 0
+            readPageAction = fmap fst $
+                runPager pagerConf pagerState $ readPage pageId
+        page <- evalMockIO readPageAction mockState
+        return $ page == correctPage
+    pagerOffset :: Num a => a
+    pagerOffset = 1
+    pageSize = pageOverhead + 4
+    pagerConf = PagerConf "" pageSize 0 1
+    pagerState = PagerState 1 NoPageId
+    pagerConfWithOffset = PagerConf "" pageSize pagerOffset 1
+    correctPage = Page (PageId 0) (B.pack "TEST") NoPageId
+    incorrectPage = Page (PageId 10) (B.pack "NOPE") NoPageId
+    correctContents = encode correctPage
+    incorrectContents = encode incorrectPage
+    contentsWithOffset = B.append (B.replicate pagerOffset '\0') correctContents
+    anyPagerException = const True :: Selector PagerException
 
-testPagerWriting =
-    it "should write pages" pending
+testPagerWriting = do
+    it "should throw an error trying to write page with NoPageId" pending
+    it "should throw an exception trying to write overflown page" pending
+    it "should write right page correctly" pending
+
+-- Test chaining NoPageId, should throw an error
+-- Test chaining to NoPageId, should set nextId and leave payload as is
+-- Test creating new page with no free pages available,
+--      should create page with id of (pagesNumber + 1)
+-- Test creating new page with free pages available,
+--      should create page with id of the first available page
+--      in a freelist
+-- Test Pager to satisfy "max pages in memory" criterion
 
 testPager = do
     testPageIdSerialization
